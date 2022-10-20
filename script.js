0960f8d4fbb7d40966a66e714d541da783bd0669
JingYee-JY <jingyeetan2005@gmail.com>
2022-10-20 12:35:19 +0800

    Update

--- script.js
+++ script.js
@@ -5,175 +5,109 @@ const easy = document.querySelector(".easy");
 const normal = document.querySelector(".normal");
 const hard = document.querySelector(".hard");
 const game = document.querySelector(".game");
+const gameBackground = document.querySelector(".game-background");
 const ball = document.querySelector(".ball");
 const readyButton = document.querySelector(".readyButton");
 const ready = document.querySelector(".ready");
 const timerCount = document.querySelector(".timer-count");
+const redLine = document.querySelector(".redLine");
 const final= document.querySelector(".final");
 const background = document.querySelector(".background");
+const endBallImage = document.querySelector(".endBallImage");
+const encouarge= document.querySelector(".encouarge");
+const playAgain = document.querySelector(".playAgain");
+const homeButton = document.querySelector(".home");
+
+const clickSound = document.getElementById("click")
+const completed = document.getElementById("completed")
+const lose = document.getElementById("lose")
 
 let startGame = false;
 let time;
 let touch;
-let currentHit;
-Lowhit = {step: 0.5}
-Mediumhit = {step: 1}
-fall = {step: 0.2}
-leftRight = {step: 1}
+let difficulty
+let Lowhit
+let Mediumhit
+let fall
+let leftRight
 let hitforce
 let direction
-let totalHit = 5;
-let ballSize;
+let ballWidth
+let offSetY
+let endLine
+
 startButton.addEventListener("click", () => {
-    start.classList.add("hide")
-    selection.classList.remove("hide")
+    playClickSound()
+    let delay = setTimeout(() => {
+        start.classList.add("hide")
+        selection.classList.remove("hide")
+        time = 30
+    }, 200);
 })
 easy.addEventListener("click", () => {
-    selection.classList.add("hide")
-    game.classList.remove("hide")
-    time = 30
-    timerCount.innerHTML = `${time} s`;
-    ready.classList.remove("hide")
+    playClickSound()
+    let delay = setTimeout(() => {
+        difficulty = 1;
+        getReady()
+    }, 200);
 })
 normal.addEventListener("click", () => {
-    selection.classList.add("hide")
-    game.classList.remove("hide")
-    time = 50
-    timerCount.innerHTML = `${time} s`;
-    ready.classList.remove("hide")
+    playClickSound()
+    let delay = setTimeout(() => {
+        difficulty = 2;
+        getReady()
+    }, 200);
+
 })
 hard.addEventListener("click", () => {
-    selection.classList.add("hide")
-    game.classList.remove("hide")
-    time = agianTime  = 70
-    timerCount.innerHTML = `${time} s`;
-    ready.classList.remove("hide")
+    playClickSound()
+    let delay = setTimeout(() => {
+        difficulty = 3;
+        getReady()
+    }, 200);
 })
 
 readyButton.addEventListener("click", () => {
-    start.classList.add("hide")
-    began()
+    playClickSound()
+    let delay = setTimeout(() => {
+        start.classList.add("hide")
+        began()
+    }, 200);
 })
+
+playAgain.addEventListener("click", () => {
+    playClickSound()
+    let delay = setTimeout(() => {
+        final.classList.add("hide")
+        start.classList.remove("hide")
+    }, 200);
+})
+
+homeButton.addEventListener("click", () => {
+    playClickSound()
+    let delay = setTimeout(() => {
+      location.assign('https://gimme.sg/activations/dementia/');
+    }, 200);
+})
+
+function getReady(){
+    selection.classList.add("hide")
+    game.classList.remove("hide")
+    timerCount.innerHTML = `${time} s`;
+    ready.classList.remove("hide")
+    spawnBall()
+}
 
 function began(){
     ready.classList.add("hide")
     startGame = true
     ballSize = 1
-    currentHit = 0;
-    ball.innerHTML = `
-    <img class="size" src="./img/Paper Ball 1.png">`
-    spawnBall()
     beganFalling()
 }
 
 ball.addEventListener("click", () => {
     if(startGame == true && ballSize != 5){
-        currentHit = currentHit + 1
-        let changeBallSize = document.querySelector(".size");
-        if(ballSize == 1){
-            if(currentHit == 1){
-                changeBallSize.style.width = "48vh"; 
-            }
-            if(currentHit == 2){
-                changeBallSize.style.width = "46vh"; 
-            }
-            if(currentHit == 3){
-                changeBallSize.style.width = "44vh"; 
-            }
-            if(currentHit == 4){
-                changeBallSize.style.width = "42vh"; 
-            }
-            if(currentHit == 5){
-                changeBallSize.style.width = "40vh"; 
-            }
-        }
-        if(ballSize == 2){
-            if(currentHit == 1){
-                changeBallSize.style.width = "38vh"; 
-            }
-            if(currentHit == 2){
-                changeBallSize.style.width = "36vh"; 
-            }
-            if(currentHit == 3){
-                changeBallSize.style.width = "34vh"; 
-            }
-            if(currentHit == 4){
-                changeBallSize.style.width = "32vh"; 
-            }
-            if(currentHit == 5){
-                changeBallSize.style.width = "30vh"; 
-            }
-        }
-        if(ballSize == 3){
-            if(currentHit == 1){
-                changeBallSize.style.width = "28vh"; 
-            }
-            if(currentHit == 2){
-                changeBallSize.style.width = "26vh"; 
-            }
-            if(currentHit == 3){
-                changeBallSize.style.width = "24vh"; 
-            }
-            if(currentHit == 4){
-                changeBallSize.style.width = "22vh"; 
-            }
-            if(currentHit == 5){
-                changeBallSize.style.width = "20vh"; 
-            }
-        }
-        if(ballSize == 4){
-            if(currentHit == 1){
-                changeBallSize.style.width = "18vh"; 
-            }
-            if(currentHit == 2){
-                changeBallSize.style.width = "16vh"; 
-            }
-            if(currentHit == 3){
-                changeBallSize.style.width = "14vh"; 
-            }
-            if(currentHit == 4){
-                changeBallSize.style.width = "12vh"; 
-            }
-            if(currentHit == 5){
-                changeBallSize.style.width = "10vh"; 
-            }
-        }
-        console.log("c " + currentHit)
-        if(currentHit == totalHit){
-            ballSize = ballSize + 1
-            if(ballSize == 2){
-                console.log("2")
-                ball.innerHTML = `
-                <img class="size" src="./img/Paper Ball 2.png">`
-                currentHit = 0
-                changeBallSize = document.querySelector(".size");
-                changeBallSize.style.width = "40vh";  
-            }
-            if(ballSize == 3){
-                console.log("3")
-                ball.innerHTML = `
-                <img class="size" src="./img/Paper Ball 3.png">`
-                currentHit = 0
-                changeBallSize = document.querySelector(".size");
-                changeBallSize.style.width = "30vh";  
-            }
-            if(ballSize == 4){
-                console.log("4")
-                ball.innerHTML = `
-                <img class="size" src="./img/Paper Ball 4.png">`
-                currentHit = 0
-                changeBallSize = document.querySelector(".size");
-                changeBallSize.style.width = "20vh";  
-            }
-            if(ballSize == 5){
-                console.log("5")
-                ball.innerHTML = `
-                <img class="size" src="./img/Paper Ball 5.png">`
-                currentHit = 0
-                changeBallSize = document.querySelector(".size"); 
-                changeBallSize.style.width = "10vh"; 
-            }
-        }
+        playClickSound()
         touch = true;
         hitforce = Math.random() > 0.5 ? 1 : 2
         direction = Math.random() > 0.5 ? 1 : 2
@@ -187,10 +121,66 @@ function beganFalling(){
 }
 
 function spawnBall(){
-    let border = game.getBoundingClientRect();
+    let border = gameBackground.getBoundingClientRect();
     ball.y = 0;
-    ball.x = (border.width / 2) - 200
-    console.log(border.width)
+    if(border.width > 768){
+        offSetY = 200
+        if(difficulty == 1){
+            ballWidth = 400
+            Lowhit = 4.5
+            Mediumhit = 5
+            fall = 4
+            leftRight = 4
+            endLine = border.height - 100
+        }
+        if(difficulty == 2){
+            ballWidth = 325
+            Lowhit = 5.5
+            Mediumhit = 6
+            fall = 5
+            leftRight = 5
+            endLine = border.height - 160
+        }
+        if(difficulty == 3){
+            ballWidth = 300
+            Lowhit = 6.5
+            Mediumhit = 7
+            fall = 6
+            leftRight = 6
+            endLine = border.height - 220
+        }
+    }
+    if(border.width < 768){
+        offSetY = 125
+        if(difficulty == 1){
+            ballWidth = 250
+            Lowhit = 2.5
+            Mediumhit = 3
+            fall = 2
+            leftRight = 2
+            endLine = border.height - 50
+        }
+        if(difficulty == 2){
+            ballWidth = 225
+            Lowhit = 3.5
+            Mediumhit = 4
+            fall = 3
+            leftRight = 3
+            endLine = border.height - 80
+        }
+        if(difficulty == 3){
+            ballWidth = 200
+            Lowhit = 4.5
+            Mediumhit = 5
+            fall = 4
+            leftRight = 4
+            endLine = border.height - 110
+        }
+    }
+    ball.style.width = ballWidth + "px";
+    ball.style.height = ballWidth + "px";
+    redLine.style.top = endLine + 'px';
+    ball.x = (border.width / 2) - offSetY
     ball.style.top = ball.y + 'px';
     ball.style.left = ball.x + 'px';
 }
@@ -201,78 +191,49 @@ function fallingBall(){
     }
 }
 function moveBall(){
+    console.log(fall, Lowhit, Mediumhit)
     let border = background.getBoundingClientRect();
     if(direction == 1){
-        ball.x = ball.x - leftRight.step
+        ball.x = ball.x - leftRight
         ball.style.left = ball.x + 'px';
-        if(ball.x < - 25){
+        if(ball.x < 0){
             direction = 2
         }
     }
     if(direction == 2){
-        ball.x = ball.x + leftRight.step
+        ball.x = ball.x + leftRight
         ball.style.left = ball.x + 'px';
-        if(ball.x > (border.width - 200)){
+        if(ball.x > Math.floor(border.width - ballWidth)){
             direction = 1
         }
     }
     if(touch == true){
         if(hitforce == 1){
-            ball.y = ball.y - Lowhit.step
-            ball.style.top = ball.y + "vh"
+            ball.y = ball.y - Lowhit
+            ball.style.top = ball.y + "px"
             if(ball.y < 0){
                 touch = false
             }
+            return
         }    
         if(hitforce == 2){
-            ball.y = ball.y - Mediumhit.step
-            ball.style.top = ball.y + "vh"
+            ball.y = ball.y - Mediumhit
+            ball.style.top = ball.y + "px"
             if(ball.y < 0){
                 touch = false
             }
+            return
         }      
     }
-    if(ball.y > 100){
+    if(ball.y > endLine){
         startGame = false
             game.classList.add("hide")
             final.classList.remove("hide")
-            let ballImage
-            if(ballSize == 1){
-                ballImage = "./img/Paper Ball 1.png"
-            }
-            if(ballSize == 2){
-                ballImage = "./img/Paper Ball 2.png"
-            }
-            if(ballSize == 3){
-                ballImage = "./img/Paper Ball 3.png" 
-            }
-            if(ballSize == 4){
-                ballImage = "./img/Paper Ball 4.png"
-            }
-            if(ballSize == 5){
-                ballImage = "./img/Paper Ball 5.png"
-            }
-            final.innerHTML = `
-            <div class="finalBackground">
-            <div class="imageText">
-            <img class="endBallImage" src="${ballImage}">
-            <p class="encouarge">So Close!</p>
-            </div>
-            </div>
-            <button class="playAgain">
-                <img class="btn" src="./img/tryAgain.png">
-            </button>
-            <button class="home" onclick="window.location.href='https://gimme.sg/activations/dementia/';">
-                <img class="btn" src="./img/home.png">
-            </button>`
-            let playAgain = document.querySelector(".playAgain");
-            playAgain.addEventListener("click", () => {
-                final.classList.add("hide")
-                start.classList.remove("hide")
-            })
+            endBallImage.src = "./img/Paper Ball 5.png"
+            encouarge.innerHTML = "So Close!"
     }
-    ball.y = ball.y + fall.step
-    ball.style.top = ball.y + "vh"
+    ball.y = ball.y + fall
+    ball.style.top = ball.y + "px"
 }
 
 function updateCountDown(){
@@ -282,44 +243,22 @@ function updateCountDown(){
             startGame = false
             game.classList.add("hide")
             final.classList.remove("hide")
-            let ballImage
-            if(ballSize == 1){
-                ballImage = "./img/Paper Ball 1.png"
-            }
-            if(ballSize == 2){
-                ballImage = "./img/Paper Ball 2.png"
-            }
-            if(ballSize == 3){
-                ballImage = "./img/Paper Ball 3.png" 
-            }
-            if(ballSize == 4){
-                ballImage = "./img/Paper Ball 4.png"
-            }
-            if(ballSize == 5){
-                ballImage = "./img/Paper Ball 5.png"
-            }
-            final.innerHTML = `
-            <div class="finalBackground">
-            <div class="imageText">
-            <img class="endBallImage" src="${ballImage}">
-            <p class="encouarge">You did it!</p>
-            <p class="tell">Keep up the good work!</p>
-            </div>
-            </div>
-            <button class="playAgain">
-            <img class="btn" src="./img/tryAgain.png">
-            </button>
-            <button class="home" onclick="window.location.href='https://gimme.sg/activations/dementia/';">
-                <img class="btn" src="./img/home.png">
-            </button>`
-            let playAgain = document.querySelector(".playAgain");
-            playAgain.addEventListener("click", () => {
-                final.classList.add("hide")
-                start.classList.remove("hide")
-            })
+            endBallImage.src = "./img/Paper Ball 1.png"
+            encouarge.innerHTML = "You did it!"
         }
         time--;
     }
 }
 
 setInterval(updateCountDown, 1000)
+
+function playClickSound(){
+    console.log(clickSound)
+    clickSound.currentTime = 0
+    clickSound.play()
+}
+
+/*prevent double tag zoom*/
+document.addEventListener('dblclick', function(event) {
+event.preventDefault();
+}, { passive: false });
